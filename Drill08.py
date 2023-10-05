c6bf25a1861fedc62a000ab7c89a0af3569f97ed
2002OID <ykkim308321@gmail.com>
2023-10-05 11:15:49 +0900

    큰 공 클래스 생성

--- Drill08.py
+++ Drill08.py
@@ -26,13 +26,25 @@ class Boy:
     def draw(self):
         self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
-class Ball:
+class Small_Ball:
     def __init__(self):
         self.x, self.y = random.randint(0, 800), 599
         self.image = load_image('ball21x21.png')
 
     def update(self):
         if(self.y > 60):
+            self.y -= 5
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+class Big_Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 800), 599
+        self.image = load_image('ball41x41.png')
+
+    def update(self):
+        if(self.y > 70):
             self.y -= 5
 
     def draw(self):
@@ -52,16 +64,21 @@ def reset_world():
     global grass
     global team
     global world
-    global balls
+    global small_balls
+    global big_balls
+
     running = True
     world = []
     grass = Grass()
     world.append(grass)
     #team = [Boy() for i in range(11)]
     #world += team
-    balls = [Ball() for i in range(11)]
-    world += balls
-
+    sb = random.randint(0,20 + 1)
+    small_balls = [Small_Ball() for i in range(sb)]
+    world += small_balls
+    bb = 20 - sb
+    big_balls = [Big_Ball() for i in range(bb)]
+    world += big_balls
 def update_world():
     for o in world:
         o.update()
